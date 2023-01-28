39b77d37bee8f57661e2c835d69f7912bf2bd6ce
Oleg <legyss@mail.ru>
2023-01-28 21:54:20 +0600

    modified Task_25

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -3,57 +3,63 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16 */
 
-int[] EnteringNumbers()
+int a;
+int b;
+EnteringNumbers();
+Console.WriteLine("Число A ({0}) возведенное в степень B ({1}) равно {2}", a, b, Pow(a, b));
+
+
+void EnteringNumbers()
 {
     int[] numbers = new int[2];
     string[] subStr;
-link:
+
     while (true)
     {
         Console.WriteLine("Введите целое число A и натуральное число B через запятую: ");
         string str = Console.ReadLine() ?? String.Empty;
         subStr = str.Split(',');
-        if (subStr.Length == 2) break;
-        Console.WriteLine("Ошибка! Формат ввода не соответствует заданному.");
-        Console.WriteLine("************************************************");
-    }
-
-    string[] info = { "A", "B" };
-    for (int i = 0; i < numbers.Length; i++)
-    {
-
-        if (int.TryParse(subStr[i], out numbers[i]))
+        if (subStr.Length == 2)
         {
-            if (i == 1) // Проверяем второе число на натуральность
-                if (numbers[i] <= 0)
+            if (int.TryParse(subStr[0], out a))
+            {
+                if (int.TryParse(subStr[1], out b))
                 {
-                    Console.WriteLine("Ошибка! Число " + info[i] + " не является натуральным.");
-                    Console.WriteLine("******************************************************");
-                    goto link;
+                    if (b > 0) break;
+                    else
+                    {
+                        Console.WriteLine("Ошибка! Число B не является натуральным.");
+                        Console.WriteLine("****************************************");
+                    }
                 }
+                else
+                {
+                    Console.WriteLine("Ошибка! Число B содержит символ.");
+                    Console.WriteLine("********************************");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Число A содержит символ.");
+                Console.WriteLine("********************************");
+
+            }
         }
         else
         {
-            Console.WriteLine("Ошибка! Число " + info[i] + " содержит символ.");
-            Console.WriteLine("**********************************************");
-            goto link;
+            Console.WriteLine("Ошибка! Формат ввода не соответствует заданному.");
+            Console.WriteLine("************************************************");
         }
     }
-    return numbers;
 }
 
-string Pow(int[] num)
+int Pow(int a, int b)
 {
     string str = String.Empty;
     int mul = 1;
-    for (int i = 1; i <= num[1]; i++)
-        mul *= num[0];
-
-    if (mul == 0 && num[0] != 0 || mul < 0 && num[0] > 0)
-        str = "Ошибка! Переполнение разрядной сетки (-2 147 483 648 ... 2 147 483 647)";
-    else str = $"Число A {num[0]} возведенное в степень B {num[1]} равно {mul}";
-
-    return str;
+    for (int i = 1; i <= b; i++)
+        mul *= a;
+    return mul;
 }
 
-Console.WriteLine(Pow(EnteringNumbers()));
+
