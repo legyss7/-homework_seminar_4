349fb8ca764b42b00e3b55efccc054f42f41db16
Oleg <legyss@mail.ru>
2023-01-28 13:06:33 +0600

    added Task_27

---
+++ Task_27/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 27: Напишите программу, которая принимает на
+ вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+int EnteringNumbers()
+{
+    int num;
+    Console.Write("Введите целое число: ");
+    while (! int.TryParse(Console.ReadLine(), out num))
+    {
+        Console.WriteLine("Ошибка! Введенное число содержит символ.");
+        Console.WriteLine("****************************************");
+        Console.Write("Введите целое число: ");
+    }
+    return num;
+}
+
+int Sum(int num)
+{
+    int sum = 0;
+    num = Math.Abs(num);
+    while(num != 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Сумма цифр во введенном числе равна {0}", Sum(EnteringNumbers()));
